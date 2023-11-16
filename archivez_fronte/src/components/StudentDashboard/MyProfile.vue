99fedb850c05a6aef3361cadeb57681f2b4a6037
Charmel Montellano <144670521+montellanocharmel@users.noreply.github.com>
2023-11-16 18:48:32 +0800

    commit

--- archivez_fronte/src/components/StudentDashboard/MyProfile.vue
+++ archivez_fronte/src/components/StudentDashboard/MyProfile.vue
@@ -461,48 +461,25 @@
 
   
 <script>
-  export default {
-    name: "DashboardStyle",
-
-    mounted() {
-        const scriptPaths = [
-
-            '/purplejs/vendors/js/vendor.bundle.base.js',
-            '/purplejs/vendors/chart.js/Chart.min.js',
-            '/purplejs/js/jquery.cookie.js',
-            '/purplejs/js/off-canvas.js',
-            '/purplejs/js/hoverable-collapse.js',
-            '/purplejs/js/dashboard.js',
-            '/purplejs/js/todolist.js',
-       
-        ];
-        
-
-        const loadScript = (path) => {
-        return new Promise((resolve, reject) => {
-            const script = document.createElement('script');
-            script.src = path;
-            script.onload = resolve;
-            script.onerror = reject;
-            document.body.appendChild(script);
-        });
-        };
-
-        const loadScripts = async () => {
-        try {
-            await loadScript(scriptPaths[0]);
-            for (let i = 1; i < scriptPaths.length; i++) {
-            await loadScript(scriptPaths[i]);
-            }
-        } catch (error) {
-            console.error('Failed to load scripts:', error);
-        }
-        };
-        loadScripts();
+import axios from 'axios'
+export default{
+  data(){
+    return{
+      info:[],
     }
-   
-  };
-  
+  },
+  created(){
+    this.getInfo();
+  },
+  methods:{
+    async getInfo(){
+      try{
+        const inf = await axios.get('getStudentProfileData');
+      this.info = inf.data;
+      } catch (error){
+        console.log(error);
+      }
+    }
+  }
+}
 </script>
-
-
