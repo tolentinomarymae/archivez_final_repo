1872340b53c396c8528f92d1d3cfc01d7a0f3207
Charmel Montellano <144670521+montellanocharmel@users.noreply.github.com>
2023-11-16 10:02:38 +0800

    commit

--- archivez_fronte/src/main.js
+++ archivez_fronte/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import axios from 'axios'
 
 loadFonts()
+axios.defaults.baseURL="http://localhost:8080/"
 
 createApp(App)
   .use(router)
