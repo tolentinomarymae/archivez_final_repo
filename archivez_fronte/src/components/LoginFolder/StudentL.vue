f530b6d2d793bbcc1691cbecd151694c7a148f8f
Bacay, Sherwin <bacaylabog@gmail.com>
2023-11-23 19:14:58 +0800

    g

--- archivez_fronte/src/components/LoginFolder/StudentL.vue
+++ archivez_fronte/src/components/LoginFolder/StudentL.vue
@@ -50,7 +50,7 @@
              <p class="mt-4">
                <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline" href="#"> Forgot your password?</a>
              </p>
-             <router-link to="/studentdashboard">Dashboard</router-link>
+             <router-link to="/studentdashboard">Dashboard </router-link>
                <router-link to="/">Back to home</router-link>
              
            </div>
